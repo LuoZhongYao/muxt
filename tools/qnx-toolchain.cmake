622e7561d81f2b0ae35ec4687ecfbb94b96a6498
LuoZhongYao <LuoZhongYao@gmail.com>
2017-05-08 18:19:26 +0800

    编译链设置

--- tools/qnx-toolchain.cmake
+++ tools/qnx-toolchain.cmake
@@ -1,20 +1,15 @@
-# this one is important
-#SET(QNX TRUE)
 SET(CMAKE_SYSTEM_NAME QNX)
 #this one not so much
 SET(CMAKE_SYSTEM_VERSION 6.6)
 
 # specify the cross compiler
-SET(QNXSDK /home/z/tools/qnx)
-SET(QNX_TARGET ${QNXSDK}/target/qnx6)
-SET(QNX_HOST   ${QNXSDK}/host/linux/x86)
-SET(CMAKE_C_COMPILER   ${QNX_HOST}/usr/bin/arm-unknown-nto-qnx6.6.0eabi-gcc)
-SET(CMAKE_CXX_COMPILER ${QNX_HOST}/usr/bin/arm-unknown-nto-qnx6.6.0eabi-g++)
+SET(CMAKE_C_COMPILER   arm-unknown-nto-qnx6.6.0eabi-gcc)
+SET(CMAKE_CXX_COMPILER arm-unknown-nto-qnx6.6.0eabi-g++)
 SET(CMAKE_SYSTEM_PROCESSOR ARMVLE-V7A)
 add_definitions(-D_QNX_SOURCE)
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  ${QNXSDK})
+SET(CMAKE_FIND_ROOT_PATH  ${QNX_HOST})
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
